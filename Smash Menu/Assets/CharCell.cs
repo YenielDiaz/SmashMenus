b6b0548e3495eaf884f6680df0b96bfebc51c60e
YenielDiaz <31051936+YenielDiaz@users.noreply.github.com>
2019-11-11 18:26:33 -0500

    Started SmashMenu
    Added Grid object
    Made CharCell prefab
    Made CharCell Scriptable Object

---
+++ Smash Menu/Assets/CharCell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Character", menuName = "character")]
+public class CharCell : ScriptableObject
+{
+    [SerializeField] string characterName;
+    [SerializeField] Sprite characterImage;
+}
---
+++ Smash Menu/Assets/SmashCSS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashCSS : MonoBehaviour
+{
+    [SerializeField] List<CharCell> chars = new List<CharCell>();
+    [SerializeField] GameObject charCellPrefab;
+
+    void Start()
+    {
+
+    }
+
+    private void SpawnCharacterCell(CharCell character)
+    {
+        GameObject charCell = Instantiate(charCellPrefab, transform);
+
+        Image charImage = charCell.transform.Find("Image").GetComponent<Image>();
+        TextMeshProUGUI charText;
+    }
+}
